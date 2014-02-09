ba46e5b066520d593154702275f91942f59eb26c
Randy Burgess <wrburgess@wrburgess.com>
2014-02-08 20:07:57 -0600

    adding domain for devise emails to environments

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -77,4 +77,7 @@ App::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Devise default url config
+  config.action_mailer.default_url_options = { host: "offbook-server-staging.herokuapp.com" }
 end
